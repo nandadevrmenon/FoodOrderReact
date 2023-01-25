c8403068b6f94cc46de1cf189731edbd3166ea9b
nandadev <nandadevrmenon@gmail.com>
2023-01-25 12:22:52 +0000

    Initial Commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React,{Fragment} from "react";
+
+import classes from "./Header.module.css"
+import banner from "../../assets/banner.webp"
+import HeaderCartButton from "../UI/HeaderCartButton";
+
+const Header = (props)=>{
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>FoodZap</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src = {banner} alt ="A food flatlay"></img>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Header;
